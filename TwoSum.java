394b6de16b8f6455867b338e82b6ea3586361625
atmh <77260849+atmh@users.noreply.github.com>
2021-12-28 09:39:14 +0800

    Add files via upload

--- TwoSum.java
+++ TwoSum.java
@@ -1,13 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
   public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    int[] res = new int[2];
     for (int i = 0; i < nums.length; i++) {
-      for (int j = i+1; j < nums.length; j++) {
-        if (nums[i] + nums[j] == target) {
-          int[] ans = {i, j};
-          return ans;
-        }
+      int complement = target - nums[i];
+      if (map.get(complement) != null) {
+        res[1] = i;
+        res[0] = map.get(complement);
       }
+      map.put(nums[i], i);
     }
-    return null;
+    return res;
   }
 }
